8b322fca58f087d32b4eaeb83adf16a2838a1ed6
kristophertab <krzysiektab@gmail.com>
2020-11-08 12:08:32 +0100

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use std::fmt::Write;
+
+fn main() {
+    println!("Hello, world!");
+    println!("I'm Rustacean!");
+
+    // In general, the `{}` will be automatically replaced with any
+    // arguments. These will be stringified.
+    println!("{} days", 31);
+
+    // Without a suffix, 31 becomes an i32. You can change what type 31 is
+    // by providing a suffix. The number 31i64 for example has the type i64.
+
+    // There are various optional patterns this works with. Positional
+    // arguments can be used.
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    // As can named arguments.
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+
+    // Special formatting can be specified after a `:`.
+    println!(
+        "{} of {:b} people know binary, the other half doesn't",
+        1, 2
+    );
+
+    // You can right-align text with a specified width. This will output
+    // "     1". 5 white spaces and a "1".
+    println!("{number:>width$}", number = 1, width = 6);
+
+    // You can pad numbers with extra zeroes. This will output "000001".
+    println!("{number:>0width$}", number = 1, width = 6);
+
+    // Rust even checks to make sure the correct number of arguments are
+    // used.
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    // Derive the `fmt::Debug` implementation for `Structure`. `Structure`
+    // is a structure which contains a single `i32`.
+    #[derive(Debug)]
+    struct Structure(i32);
+
+    // Put a `Structure` inside of the structure `Deep`. Make it printable
+    // also.
+    #[derive(Debug)]
+    struct Deep(Structure);
+
+    // Printing with `{:?}` is similar to with `{}`.
+    println!("{:?} months in a year.", 12);
+    println!(
+        "{1:?} {0:?} is the {actor:?} name.",
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
+
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
+
+    // The problem with `derive` is there is no control over how
+    // the results look. What if I want this to just show a `7`?
+    println!("Now {:#?} will print!", Deep(Structure(7)));
+}
